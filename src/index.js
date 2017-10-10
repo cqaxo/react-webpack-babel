83170edbacbb2ea2e0589c8de6dfd23b1bb5692b
Jacob S <cqaxo@tuta.io>
2017-10-10 18:13:33 -0400

    Edited index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
 
-// Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept("./app", () => {
-    const NextApp = require("./app").default;
-    ReactDOM.render(
-      <AppContainer>
-        <NextApp />
-      </AppContainer>,
-      document.getElementById("app")
-    );
-  });
-}
+
