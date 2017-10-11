f8062f3bfbccadfdc9ab345ea0e2f4e5fc2f7d6c
Jacob S <cqaxo@tuta.io>
2017-10-10 21:37:49 -0400

    Update webpack.production.config.js

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -13,7 +13,7 @@ loaders.push({
 
 module.exports = {
   entry: [
-    './src/index.jsx',
+    './src/index.js',
     './styles/index.scss'
   ],
   output: {
@@ -48,7 +48,7 @@ module.exports = {
       allChunks: true
     }),
     new HtmlWebpackPlugin({
-      template: './src/template.html',
+      template: './index.html',
       files: {
         css: ['style.css'],
         js: ['bundle.js'],
